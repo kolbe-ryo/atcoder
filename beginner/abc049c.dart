5646b32ce6ce3d8dfcdd11a2e58b468a88f22b9e
kolbe <water.northground@gmail.com>
2024-01-23 23:59:56 +0900

    2

--- beginner/abc049c.dart
+++ beginner/abc049c.dart
@@ -2,14 +2,14 @@ import 'dart:io';
 
 void main(List<String> args) {
   var s = stdin.readLineSync()!;
-  const t0 = 'dreameraser';
-  const t1 = 'dreamerase';
+  // const t0 = 'dreameraser';
+  // const t1 = 'dreamerase';
   const t2 = 'dreamer';
   const t3 = 'dream';
   const t4 = 'eraser';
   const t5 = 'erase';
 
-  const _list = [t0, t1, t2, t3, t4, t5];
+  const _list = [t4, t5, t2, t3];
 
   for (final i in _list) {
     s = s.replaceAll(RegExp(i), '');
@@ -18,5 +18,5 @@ void main(List<String> args) {
       return;
     }
   }
-  print('No');
+  print('NO');
 }
