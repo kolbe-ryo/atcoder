c86adbcaf116cbc85fdba21bb9b2238e22ae9d6e
kolbe <water.northground@gmail.com>
2024-01-23 23:21:08 +0900

    白昼夢

---
+++ beginner/abc049c.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  var s = stdin.readLineSync()!;
+  const t0 = 'dreameraser';
+  const t1 = 'dreamerase';
+  const t2 = 'dreamer';
+  const t3 = 'dream';
+  const t4 = 'eraser';
+  const t5 = 'erase';
+
+  const _list = [t0, t1, t2, t3, t4, t5];
+
+  for (final i in _list) {
+    s = s.replaceAll(RegExp(i), '');
+    if (s.isEmpty) {
+      print('YES');
+      return;
+    }
+  }
+  print('No');
+}
